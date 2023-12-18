ae7280942c79b923071aeb3e5ffcfc0fc6218b2d
Salma-101 <145042219+Salma-101@users.noreply.github.com>
2023-12-18 12:01:59 +0530

    Final MainPage

--- MainPage.py
+++ MainPage.py
@@ -22,6 +22,18 @@ def Generate_New_Patient_Profile():
 def Patient_Info():
     import Patient_Info
 
+def UpdateData():
+    import UpdatePatientData
+
+def ViewData():
+    import TreeViewData
+
+def TestResults():
+    __import__('Patient Test Results.py')
+
+def Invoice():
+    import InvoiceByDhatri
+
 b1 = Button(win, text="Generate New Patient Profile", fg="green", font=("Times New Roman",13), command = Generate_New_Patient_Profile)
 b1.pack()
 empty = Label(win, text=" ",font=1)
@@ -30,15 +42,19 @@ b2 = Button(win, text="Enter Patient Info", fg="green", font=("Times New Roman",
 b2.pack()
 empty = Label(win, text="",font=1)
 empty.pack()
-b3 = Button(win, text="Update Patient Data", fg="green", font=("Times New Roman",13))
+b3 = Button(win, text="Update Patient Data", fg="green", font=("Times New Roman",13), command=UpdateData)
 b3.pack()
 empty = Label(win, text="",font=1)
 empty.pack()
-b4 = Button(win, text="Patient Test Results", fg="green", font=("Times New Roman",13))
+b4 = Button(win, text="View Patients Data", fg="green", font=("Times New Roman",13),command=ViewData)
 b4.pack()
 empty = Label(win, text="",font=1)
 empty.pack()
-b5 = Button(win, text="Patient Invoice", fg="green", font=("Times New Roman",13))
+b5 = Button(win, text="Patient Test Results", fg="green", font=("Times New Roman",13), command=TestResults)
 b5.pack()
+empty = Label(win, text="",font=1)
+empty.pack()
+b6 = Button(win, text="Patient Invoice", fg="green", font=("Times New Roman",13),command=Invoice)
+b6.pack()
 
 win.mainloop()
